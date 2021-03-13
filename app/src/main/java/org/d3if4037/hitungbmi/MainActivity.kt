86f064249491c6cd805770e9c3282d2016a217e0
adhitia_dharma_putri <adhitiaputri16@gmail.com>
2021-03-13 21:36:03 +0700

    Refactor fitur hitung BMI ke fragment

--- app/src/main/java/org/d3if4037/hitungbmi/MainActivity.kt
+++ app/src/main/java/org/d3if4037/hitungbmi/MainActivity.kt
@@ -2,70 +2,11 @@ package org.d3if4037.hitungbmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.widget.Toast
-import org.d3if4037.hitungbmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.button.setOnClickListener{ hitungBMI()}
-        binding.resetBt.setOnClickListener { reset() }
-    }
-
-    private fun reset() {
-        var berat = binding.isiBerat.text.toString()
-        berat = ""
-        var tinggi = binding.isiTinggi.text.toString()
-        tinggi = ""
-    }
-
-
-    private fun hitungBMI(){
-        val berat = binding.isiBerat.text.toString()
-        if(TextUtils.isEmpty(berat)){
-            Toast.makeText(this, R.string.berat_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-        val tinggi = binding.isiTinggi.text.toString()
-        if(TextUtils.isEmpty(tinggi)){
-            Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-        val tinggiCm = tinggi.toFloat() / 100
-
-        val selectedId = binding.rg.checkedRadioButtonId
-        if (selectedId == -1){
-            Toast.makeText(this, R.string.gender_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-        val isMale = selectedId == R.id.priaRb
-        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
-        val kategori = getKategori(bmi, isMale)
-
-        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
-        binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
-    }
-
-    private fun getKategori(bmi: Float, ismale: Boolean): String {
-        val stringRes = if (ismale){
-            when{
-                bmi < 20.5 -> R.string.kurus
-                bmi >= 27.0 -> R.string.gemuk
-                else -> R.string.ideal
-            }
-        }else{
-            when{
-                bmi < 18.5 -> R.string.kurus
-                bmi >= 25.0 -> R.string.gemuk
-                else -> R.string.ideal
-            }
-        }
-        return getString(stringRes)
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/org/d3if4037/hitungbmi/ui/HitungFragment.kt
@@ -0,0 +1,67 @@
+package org.d3if4037.hitungbmi.ui
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import org.d3if4037.hitungbmi.R
+import org.d3if4037.hitungbmi.databinding.FragmentHitungBinding
+
+class HitungFragment : Fragment() {
+
+    private lateinit var binding: FragmentHitungBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentHitungBinding.inflate(layoutInflater,
+                container, false)
+        binding.button.setOnClickListener { hitungBMI() }
+        return binding.root
+    }
+        private fun hitungBMI() {
+            val berat = binding.isiBerat.text.toString()
+            if (TextUtils.isEmpty(berat)) {
+                Toast.makeText(context, R.string.berat_invalid, Toast.LENGTH_LONG).show()
+                return
+            }
+            val tinggi = binding.isiTinggi.text.toString()
+            if (TextUtils.isEmpty(tinggi)) {
+                Toast.makeText(context, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
+                return
+            }
+            val tinggiCm = tinggi.toFloat() / 100
+
+            val selectedId = binding.rg.checkedRadioButtonId
+            if (selectedId == -1) {
+                Toast.makeText(context, R.string.gender_invalid, Toast.LENGTH_LONG).show()
+                return
+            }
+            val isMale = selectedId == R.id.priaRb
+            val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
+            val kategori = getKategori(bmi, isMale)
+
+            binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
+            binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
+        }
+
+        private fun getKategori(bmi: Float, ismale: Boolean): String {
+            val stringRes = if (ismale) {
+                when {
+                    bmi < 20.5 -> R.string.kurus
+                    bmi >= 27.0 -> R.string.gemuk
+                    else -> R.string.ideal
+                }
+            } else {
+                when {
+                    bmi < 18.5 -> R.string.kurus
+                    bmi >= 25.0 -> R.string.gemuk
+                    else -> R.string.ideal
+                }
+            }
+            return getString(stringRes)
+        }
+    }
+
+
