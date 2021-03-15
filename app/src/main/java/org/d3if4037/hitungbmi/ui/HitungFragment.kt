87248bf9ed87f5f784376d2e4420e8310605f41b
adhitia_dharma_putri <adhitiaputri16@gmail.com>
2021-03-15 14:27:15 +0700

    Membuat navigasi menggunakan menu

--- app/src/main/java/org/d3if4037/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if4037/hitungbmi/ui/HitungFragment.kt
@@ -2,12 +2,11 @@ package org.d3if4037.hitungbmi.ui
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if4037.hitungbmi.R
 import org.d3if4037.hitungbmi.data.KategoriBmi
 import org.d3if4037.hitungbmi.databinding.FragmentHitungBinding
@@ -16,67 +15,86 @@ class HitungFragment : Fragment() {
 
     private lateinit var binding: FragmentHitungBinding
     private lateinit var kategoriBmi: KategoriBmi
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        binding = FragmentHitungBinding.inflate(layoutInflater,
-                container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHitungBinding.inflate(
+            layoutInflater,
+            container, false
+        )
         binding.button.setOnClickListener { hitungBMI() }
         binding.saranbutton.setOnClickListener { view: View ->
             view.findNavController().navigate(
-                HitungFragmentDirections.
-                actionHitungFragmentToSaranFragment(kategoriBmi)
+                HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi)
             )
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
-        private fun hitungBMI() {
-            val berat = binding.isiBerat.text.toString()
-            if (TextUtils.isEmpty(berat)) {
-                Toast.makeText(context, R.string.berat_invalid, Toast.LENGTH_LONG).show()
-                return
-            }
-            val tinggi = binding.isiTinggi.text.toString()
-            if (TextUtils.isEmpty(tinggi)) {
-                Toast.makeText(context, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
-                return
-            }
-            val tinggiCm = tinggi.toFloat() / 100
 
-            val selectedId = binding.rg.checkedRadioButtonId
-            if (selectedId == -1) {
-                Toast.makeText(context, R.string.gender_invalid, Toast.LENGTH_LONG).show()
-                return
-            }
-            val isMale = selectedId == R.id.priaRb
-            val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
-            val kategori = getKategori(bmi, isMale)
-
-            binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
-            binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
-            binding.saranbutton.visibility = View.VISIBLE
+    private fun hitungBMI() {
+        val berat = binding.isiBerat.text.toString()
+        if (TextUtils.isEmpty(berat)) {
+            Toast.makeText(context, R.string.berat_invalid, Toast.LENGTH_LONG).show()
+            return
         }
-
-        private fun getKategori(bmi: Float, ismale: Boolean): String {
-            kategoriBmi= if(ismale){
-                when {
-                    bmi < 20.5 -> KategoriBmi.KURUS
-                    bmi >= 27.0 -> KategoriBmi.GEMUK
-                    else -> KategoriBmi.IDEAL
-                }
-            } else {
-                when {
-                    bmi < 18.5 -> KategoriBmi.KURUS
-                    bmi >= 25.0 -> KategoriBmi.GEMUK
-                    else -> KategoriBmi.IDEAL
-                }
-            }
-            val stringRes = when (kategoriBmi){
-                KategoriBmi.KURUS -> R.string.kurus
-                KategoriBmi.IDEAL -> R.string.ideal
-                KategoriBmi.GEMUK -> R.string.gemuk
-            }
-            return getString(stringRes)
+        val tinggi = binding.isiTinggi.text.toString()
+        if (TextUtils.isEmpty(tinggi)) {
+            Toast.makeText(context, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
+            return
         }
+        val tinggiCm = tinggi.toFloat() / 100
+
+        val selectedId = binding.rg.checkedRadioButtonId
+        if (selectedId == -1) {
+            Toast.makeText(context, R.string.gender_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val isMale = selectedId == R.id.priaRb
+        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
+        val kategori = getKategori(bmi, isMale)
+
+        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
+        binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
+        binding.saranbutton.visibility = View.VISIBLE
     }
+
+    private fun getKategori(bmi: Float, ismale: Boolean): String {
+        kategoriBmi = if (ismale) {
+            when {
+                bmi < 20.5 -> KategoriBmi.KURUS
+                bmi >= 27.0 -> KategoriBmi.GEMUK
+                else -> KategoriBmi.IDEAL
+            }
+        } else {
+            when {
+                bmi < 18.5 -> KategoriBmi.KURUS
+                bmi >= 25.0 -> KategoriBmi.GEMUK
+                else -> KategoriBmi.IDEAL
+            }
+        }
+        val stringRes = when (kategoriBmi) {
+            KategoriBmi.KURUS -> R.string.kurus
+            KategoriBmi.IDEAL -> R.string.ideal
+            KategoriBmi.GEMUK -> R.string.gemuk
+        }
+        return getString(stringRes)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
 
 
